2b4fa7a4af5b7bcd3ff29d23f7349731e0addcbd
Beniu <beniugalaxy@gmail.com>
2026-05-08 20:26:55 +0200

    poprawa działania hasła na stronę admina: fpvlab.pl/admin

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -8,7 +8,7 @@ import { redirect } from "next/navigation";
 export default async function AdminLayout({ children }: { children: React.ReactNode }) {
   const session = await getServerSession(authOptions);
   if (!session) {
-    redirect("/admin/login");
+    redirect("/auth/login");
   }
   return <>{children}</>;
 }
